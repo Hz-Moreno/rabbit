b3f743dc69a6247b77e88626bc49d3e088db76ba
Hezrai Chagas <hezraichagas@hotmail.com>
2025-03-22 20:05:47 -0300

    .

---
+++ app/src/main/java/com/rabbit/app/controllers/panel/Days.java
@@ -0,0 +1,9 @@
+package com.rabbit.app.controllers.panel;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Days {
+
+}
---
+++ app/src/main/java/com/rabbit/app/middlewares/JWTAuth.java
@@ -0,0 +1,5 @@
+package com.rabbit.app.middlewares;
+
+public class JWTAuth {
+
+}
